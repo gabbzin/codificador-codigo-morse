ca134f9397cde90221d78ab404af2856372563d7
Gabriel P. <cgabrielpimentacruz111@gmail.com>
2024-04-18 14:29:00 -0300

    Atualizações no codificador/descodificador de código morse
    Fiz várias alterações, como a criação de dicionários (arrays) para percorrer os valores, reduzindo as linhas de código e dando eficiência ao programa. Removi a função de som por enquanto

--- codificador/codificador.c
+++ codificador/codificador.c
@@ -1,21 +1,57 @@
 #include <stdio.h>
 #include <windows.h> // Biblioteca com sleep e o beep
+#include <string.h>
+#include <ctype.h>
 
-/*
-    Caso se trate de uma frase, existe um silêncio de três unidades entre uma palavra e outra.
-    No caso de mensagens mais longas, com mais de uma frase,
-    o intervalo entre o fim de uma sentença e o começo de outra é de sete unidades.
+const char *codMorse[] = {
+    ".-", "-...", "-.-.", "-..", ".", // A, B, C, D, E
+    "..-.", "--.", "....", "..", ".---", // F, G, H, I, J
+    "-.-", ".-..", "--", "-.", "---", // K, L, M, N, O
+    ".--.", "--.-", ".-.", "...", "-", // P, Q, R, S, T
+    "..-", "...-", ".--", "-..-", "-.--", "--..", // U, V, W, X, Y, Z
+    "-----", ".----", "..---", "...--", "....-", // 0, 1, 2, 3, 4
+    ".....", "-....", "--...", "---..", "----.", // 5, 6, 7, 8, 9
+};
 
-    Os pontos têm duração curta, de uma unidade;
-    Os traços têm duração mais longa, que duram, em média, três unidades de tempo.
-*/
+const char *decodMorse[] = {
+    "A", "B", "C", "D", "E", // .- , -... , -.-. , -.. , .
+    "F", "G", "H", "I", "J", // ..-. , --. , .... , .. , .---
+    "K", "L", "M", "N", "O", // -.- , .-.. , -- , -. , ---
+    "P", "Q", "R", "S", "T", // .--. , --.- , .-. , ... , -
+    "U", "V", "W", "X", "Y", "Z", // ..- , ...- , .-- , -..- , -.-- , --..
+    "0", "1", "2", "3", "4", // ----- , .---- , ..--- , ...-- , ....-
+    "5", "6", "7", "8", "9", // ..... , -.... , --... , ---.. , ----.
+};
 
-void somPonto(){
-    Beep(1000, 500);
+void codificarMorse (const char *mensagem) {
+    printf("Mensagem codificada para Morse: ");
+    int i = 0;
+    while (mensagem[i] != '\0'){
+        if (mensagem[i] != " "){
+            char c = toupper(mensagem[i]);
+            if (isalpha(c)){
+                printf("%s ", codMorse[c - 'A']);
+            } else if (isdigit(c)){
+                printf("%s ", codMorse[c - '0' + 26]);
+            }
+        } else {
+            printf(" ");
+        }
+        i++;
+    }
 }
 
-void somBarra(){
-    Beep(1000, 1500);
+void decodificarMorse(const char *mensagem) {
+    printf("Mensagem decodificada para linguagem natural: ");
+    char *token = strtok((char *)mensagem, " ");
+    while (token != NULL){
+        int i = 0;
+        while (strcmp(token, decodMorse[i] != 0)){
+            i++;
+        }
+        printf("%c", i < 26 ? i + 'A' : (i < 36 ? (i - 26) + 'O' : decodMorse[i][0]));
+        token = strtok(NULL, " ");
+    }
 }
 
 char espacamento(){
@@ -29,14 +65,13 @@ void sleep(int segundos){
 int main(){
     // Declaração das variáveis
     char frase[100]; // Variável para armazenar a frase
-    int i = 0; // Variável criada para percorrer o array
     int realizarOperacao; // Variável para escolher a operação a ser realizada
 
     espacamento();
 
     // Início do programa
 
-    printf("Ola, seja bem-vindo a codificador de codigo morse!! \n");
+    printf("Ola, seja bem-vindo ao codificador/descodificador de codigo morse!! \n");
     printf("Antes de comecar, eu quero te passar algumas dicas: \n\n");
 
     sleep(2);
@@ -46,10 +81,6 @@ int main(){
     sleep(1);
 
     printf("* Caso use, as letras acentuadas nao serao codificadas \n");
-
-    sleep(1);
-
-    printf("* Habilite o som do seu dispositivo, caso queira ouvir o som \n");
 
     espacamento();
     sleep(2);
@@ -63,193 +94,26 @@ int main(){
 
         espacamento();
 
-        if (realizarOperacao == 1){
-            // Pedindo que o usuário digite a frase que será codificada
-            printf("Digite uma frase: ");
-            fflush(stdin);  // Função para quebrar o input feito pelo scanf
-            fgets(frase, sizeof(frase), stdin); // Input para entrada da frase
+        // Solicitando a frase
+        printf("Digite uma frase: ");
+        fflush(stdin);  // Função para quebrar o input feito pelo scanf
+        fgets(frase, sizeof(frase), stdin); // Input para entrada da frase
 
-            sleep(2);
+        sleep(2);
 
-            // Percorrendo letra a letra da variável FRASE
-            while (frase[i] != '\0'){
-                // Transformando todo o array em letra minúscula
-                frase[i] = tolower(frase[i]);
-
-                // Código para cada letra
-                switch (frase[i]){
-                    case 'a':
-                        printf(".- ");
-                        somPonto(); somBarra();
-
-                        sleep(1);
-                        break;
-
-                    case 'b':
-                        printf("-... ");
-                        somBarra(); somPonto(); somPonto(); somPonto();
-
-                        sleep(1);
-                        break;
-
-                    case 'c':
-                        printf("-.-. ");
-                        somBarra(); somPonto(); somBarra(); somPonto();
-
-                        sleep(1);
-                        break;
-
-                    case 'd':
-                        printf("-.. ");
-                        somBarra(); somPonto(); somPonto();
-
-                        sleep(1);
-                        break;
-                        
-                    case 'e':
-                        printf(". ");
-                        somPonto();
-
-                        sleep(1);
-                        break;
-
-                    case 'f':
-                        printf("..-. ");
-                        somPonto(); somPonto(); somBarra(); somPonto();
-                        
-                        sleep(1);
-                        break;
-
-                    case 'g':
-                        printf("--.");
-                        somBarra(); somBarra(); somPonto();
-
-                        sleep(1);
-                        break;
-
-                    case 'h':
-                        printf(".... ");
-                        somPonto(); somPonto(); somPonto(); somPonto();
-                        sleep(1);
-                        break;
-
-                    case 'i':
-                        printf(".. ");
-                        somPonto(); somPonto();
-                        sleep(1);
-                        break;
-
-                    case 'j':
-                        printf(".--- ");
-                        somPonto(); somBarra(); somBarra(); somBarra();
-                        sleep(1);
-                        break;
-
-                    case 'k':
-                        printf("-.- ");
-                        somBarra(); somPonto(); somBarra();
-                        sleep(1);
-                        break;
-
-                    case 'l':
-                        printf(".-.. ");
-                        somPonto(); somBarra(); somPonto(); somPonto();
-                        sleep(1);
-                        break;
-
-                    case 'm':
-                        printf("-- ");
-                        somBarra(); somBarra();
-                        sleep(1);
-                        break;
-
-                    case 'n':
-                        printf("-. ");
-                        somBarra(); somPonto();
-                        sleep(1);
-                        break;
-
-                    case 'o':
-                        printf("--- ");
-                        somBarra(); somBarra(); somBarra();
-                        sleep(1);
-                        break;
-
-                    case 'p':
-                        printf(".--. ");
-                        somPonto(); somBarra(); somBarra(); somPonto();
-                        sleep(1);
-                        break;
-
-                    case 'q':
-                        printf("--.- ");
-                        somBarra(); somBarra(); somPonto(); somBarra();
-                        sleep(1);
-                        break;
-
-                    case 'r':
-                        printf(".-. ");
-                        somPonto(); somBarra(); somPonto();
-                        sleep(1);
-                        break;
-
-                    case 's':
-                        printf("... ");
-                        somPonto(); somPonto(); somPonto();
-                        sleep(1);
-                        break;
-
-                    case 't':
-                        printf("- ");
-                        somBarra();
-                        sleep(1);
-                        break;
-
-                    case 'u':
-                        printf("..- ");
-                        somPonto(); somPonto(); somBarra();
-                        sleep(1);
-                        break;
-
-                    case 'v':
-                        printf("...- ");
-                        somPonto(); somPonto(); somPonto(); somBarra();
-                        sleep(1);
-                        break;
-
-                    case 'w':
-                        printf(".-- ");
-                        somPonto(); somBarra(); somBarra();
-                        sleep(1);
-                        break;
-
-                    case 'x':
-                        printf("-..- ");
-                        somBarra(); somPonto(); somPonto(); somBarra();
-                        sleep(1);
-                        break;
-
-                    case 'y':
-                        printf("-.-- ");
-                        somBarra(); somPonto(); somBarra(); somBarra();
-                        sleep(1);
-                        break;
-
-                    case 'z':
-                        printf("--.. ");
-                        somBarra(); somBarra(); somPonto(); somPonto();
-                        sleep(1);
-                        break;
-                }
-                i++;
-            }
-            printf("\n\n");
-            espacamento();
+        // Opções do usuário
+        if (realizarOperacao == 1){ // Codificador
+            codificarMorse(frase);
         }
-
-        if (realizarOperacao == 3){ // Opção para encerrar o programa
-            printf("Obrigado por utilizar o nosso programa!!\n\n");
+        else if (realizarOperacao == 2){ // Decodificador
+            decodificarMorse(frase);
+        }
+        else if (realizarOperacao == 3){
+            printf("Obrigado por usar o nosso programa, adeus!");
             break;
+        }
+        else{
+            printf("Opcao invalida, vamos começar novamente");
         }
     }
     
